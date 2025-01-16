8591b5b039fcf5a65b146ccd0bdae231309836cc
Marat <maldini-83@mail.ru>
2025-01-16 15:55:58 +0300

    начато добавление дополнительной логики + оптимизация старого кода

--- service/los.py
+++ service/los.py
@@ -1,5 +1,5 @@
 import datetime
-from dataclasses import dataclass
+from enum import StrEnum
 
 from aiogram import Bot
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -7,107 +7,103 @@ from pymodbus import ModbusException, ExceptionResponse
 
 from config import TankVars, _logger
 from db.repo import LosRepo, UserRepo
-from service.mailing import Mailing
+from service.mailing import send_message
 from service.modbus import ModbusService
 
 
-@dataclass
-class Messages:
+class Messages(StrEnum):
     SENSOR_FAILURE = 'Авария датчика уровня!'
     FIRST_CRIT_LEVEL = 'Первый критический уровень, необходимо проверить работу насоса!'
     SECOND_CRIT_LEVEL = 'Второй критический уровень, ёмкость скоро переполнится!'
+    CURR_SENSOR_FAILURE = 'Авария датчика тока!'
+    HIGH_CURRENT_FAILURE = 'Превышение рабочего тока!'
+    POSSIBLY_LACK_OF_POWER = 'Возможно, насос неисправен или на него не подается питание, превышение уровня воды включения насоса!'
+    POSSIBLY_DEFECTIVE_PUMP = 'Неисправен насос или засорение водопровода - уровень жидкости не падает при работе насоса!'
 
 
-class LosService:
-    @staticmethod
-    def is_warning(level) -> bool:
-        return TankVars.warning < level <= TankVars.critical
 
-    @staticmethod
-    def is_critical(level) -> bool:
-        return TankVars.critical < level <= TankVars.high_border
+def is_warning(level) -> bool:
+    return TankVars.warning < level <= TankVars.critical
 
-    @staticmethod
-    def is_failure(level) -> bool:
-        return any([level < TankVars.low_border, level > TankVars.high_border])
+def is_critical(level) -> bool:
+    return TankVars.critical < level <= TankVars.high_border
 
-    @staticmethod
-    def is_normal(level) -> bool:
-        return TankVars.low_border <= level <= TankVars.warning
+def is_failure(level) -> bool:
+    return any([level < TankVars.low_border, level > TankVars.high_border])
 
-    @classmethod
-    def check_alarm(cls, bot: Bot, scheduler: AsyncIOScheduler):
+def is_normal(level) -> bool:
+    return TankVars.low_border <= level <= TankVars.warning
+
+async def check_alarm(bot: Bot, scheduler: AsyncIOScheduler):
+    level = LosRepo.get_last_level()
+    txt = ''
+    match level:
+        case l if is_failure(l):
+            txt = Messages.SENSOR_FAILURE
+        case l if is_warning(l):
+            txt = Messages.FIRST_CRIT_LEVEL
+        case l if is_critical(l):
+            txt = Messages.SECOND_CRIT_LEVEL
+
+    if txt:
+        users = UserRepo.get_users()
+        await send_message(txt, users, bot) 
+
+def check_level(level: float, prev_level: float, bot: Bot, scheduler: AsyncIOScheduler) -> None:
+    txt = ''
+    match prev_level, level:
+        case p, c if not is_failure(p) and is_failure(c):
+            txt = Messages.SENSOR_FAILURE
+        case p, c if not is_warning(p) and is_warning(c):
+            txt = Messages.FIRST_CRIT_LEVEL
+        case p, c if not is_critical(p) and is_critical(c):
+            txt = Messages.SECOND_CRIT_LEVEL
+    if txt:
         job = scheduler.get_job('alarm')
-        if not job:
-            level = LosRepo.get_last_level()
-            txt = ''
-            match level:
-                case l if cls.is_failure(l):
-                    txt = Messages.SENSOR_FAILURE
-                case l if cls.is_warning(l):
-                    txt = Messages.FIRST_CRIT_LEVEL
-                case l if cls.is_critical(l):
-                    txt = Messages.SECOND_CRIT_LEVEL
+        if job:
+            job.modify(
+                next_run_time=datetime.datetime.now(),
+                kwargs={'message': txt, 'users': UserRepo.get_users(), 'bot': bot}
+            )
+        else:
+            scheduler.add_job(
+                func=Mailing.send_message,
+                trigger='interval',
+                hours=1,
+                next_run_time=datetime.datetime.now(),
+                id='alarm',
+                kwargs={'message': txt, 'users': UserRepo.get_users(), 'bot': bot}
+            )
 
-            if txt:
-                scheduler.add_job(
-                    func=Mailing.send_message,
-                    trigger='interval',
-                    hours=1,
-                    next_run_time=datetime.datetime.now(),
-                    id='alarm',
-                    kwargs={'message': txt, 'users': UserRepo.get_users(), 'bot': bot}
-                )
 
-    @classmethod
-    def check_level(cls, level: float, prev_level: float, bot: Bot, scheduler: AsyncIOScheduler) -> None:
-        txt = ''
-        match prev_level, level:
-            case p, c if not cls.is_failure(p) and cls.is_failure(c):
-                txt = Messages.SENSOR_FAILURE
-            case p, c if not cls.is_warning(p) and cls.is_warning(c):
-                txt = Messages.FIRST_CRIT_LEVEL
-            case p, c if not cls.is_critical(p) and cls.is_critical(c):
-                txt = Messages.SECOND_CRIT_LEVEL
-            case p, c if not cls.is_normal(p) and cls.is_normal(c):
-                job = scheduler.get_job('alarm')
-                if job:
-                    job.remove()
-        if txt:
-            job = scheduler.get_job('alarm')
-            if job:
-                job.modify(
-                    next_run_time=datetime.datetime.now(),
-                    kwargs={'message': txt, 'users': UserRepo.get_users(), 'bot': bot}
-                )
-            else:
-                scheduler.add_job(
-                    func=Mailing.send_message,
-                    trigger='interval',
-                    hours=1,
-                    next_run_time=datetime.datetime.now(),
-                    id='alarm',
-                    kwargs={'message': txt, 'users': UserRepo.get_users(), 'bot': bot}
-                )
+async def poll_registers(cls, bot: Bot, scheduler: AsyncIOScheduler):
+    def dc_accident_notification(data: int):
+        match data:
+            case 1:
+                text = 
+        
+    await ModbusService.client.connect()
+    try:
+        data = await ModbusService.client.read_holding_registers(512, 3, 16)
+        level = ModbusService.convert_to_float(data.registers[:2])
+        prev_level = LosRepo.get_last_level()[0]['level']
+        cls.check_level(level, prev_level, bot, scheduler)
+        LosRepo.write_level(level)
 
-    @classmethod
-    async def poll_registers(cls, bot: Bot, scheduler: AsyncIOScheduler):
-        await ModbusService.client.connect()
-        try:
-            data = await ModbusService.client.read_holding_registers(512, 3, 16)
-            level = ModbusService.convert_to_float(data.registers[:2])
-            prev_level = LosRepo.get_last_level()[0]['level']
-            cls.check_level(level, prev_level, bot, scheduler)
-            LosRepo.write_level(level)
-        except ModbusException as exc:
-            _logger.error(f'1 {exc}')
-            ModbusService.client.close()
-            return
-        if data.isError():
-            _logger.error(data)
-            ModbusService.client.close()
-            return
-        if isinstance(data, ExceptionResponse):
-            _logger.error(f' 2 {data}')
-            ModbusService.client.close()
+        match data.registers[-1]:
+            case 1:
+                pass
+            case 2: 
+                pass
+    except ModbusException as exc:
+        _logger.error(f'1 {exc}')
         ModbusService.client.close()
+        return
+    if data.isError():
+        _logger.error(data)
+        ModbusService.client.close()
+        return
+    if isinstance(data, ExceptionResponse):
+        _logger.error(f' 2 {data}')
+        ModbusService.client.close()
+    ModbusService.client.close()
--- service/mailing.py
+++ service/mailing.py
@@ -3,10 +3,8 @@ from aiogram.types import InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-class Mailing:
-    @staticmethod
-    async def send_message(message: str, users: list, bot: Bot):
-        builder = InlineKeyboardBuilder()
-        builder.add(InlineKeyboardButton(text='Сброс', callback_data='reset_warning'))
-        for user in users:
-            await bot.send_message(chat_id=user['id'], text=message, reply_markup=builder.as_markup())
+async def send_message(message: str, users, bot: Bot):
+    builder = InlineKeyboardBuilder()
+    builder.add(InlineKeyboardButton(text='Сброс', callback_data='reset_warning'))
+    for user in users:
+        await bot.send_message(chat_id=user['id'], text=message, reply_markup=builder.as_markup())
